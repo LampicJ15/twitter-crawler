2967e65c3a853c8849506f64af640e0d4b5333dd
LampicJ15 <jan.lampic13@gmail.com>
2023-02-07 19:52:07 +0100

    Added command line arguments for running separate actions import, export and crawl. Simplified import functionality. Fixed crawlers handling of errors from the twitter api. Updated node.jsonl data, to not contain empty nodes.

--- internal/twitter/api.go
+++ internal/twitter/api.go
@@ -30,8 +30,6 @@ func SetupClient(apiToken string) twitterClient {
 	}}
 }
 
-// user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld&
-//tweet.fields=attachments,author_id,context_annotations,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,non_public_metrics,organic_metrics,possibly_sensitive,promoted_metrics,public_metrics,referenced_tweets,reply_settings,source,text,withheld'
 func (client *twitterClient) LookupUsersByUsernames(ctx context.Context, usernames []string) {
 	lookUpOpts := twitter.UserLookupOpts{UserFields: []twitter.UserField{twitter.UserFieldCreatedAt,
 		twitter.UserFieldDescription,
